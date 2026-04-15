99356020e083149b4296bca1018ee6e772267d07
Na Hua <nahua3730@gmail.com>
2026-04-15 17:27:37 -0400

    Add: valid-palindrome-ii - submission-1

---
+++ Data Structures & Algorithms/valid-palindrome-ii/submission-1.py
@@ -0,0 +1,17 @@
+class Solution:
+    def validPalindrome(self, s: str) -> bool:
+        def is_pal(l,r):
+            while l<r:
+                if s[l]!=s[r]:
+                    return False
+                l+=1
+                r-=1
+            return True
+        left=0
+        right=len(s)-1
+        while left<right:
+            if s[left]!=s[right]:
+                return is_pal(left+1,right) or is_pal(left,right-1)
+            left+=1
+            right-=1
+        return True 
