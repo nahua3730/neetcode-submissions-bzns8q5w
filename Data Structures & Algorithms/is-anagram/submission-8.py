c724381860dc008525a9839405fd092e11330788
Na Hua <nahua3730@gmail.com>
2026-04-27 00:40:43 -0400

    Add: is-anagram - submission-8

---
+++ Data Structures & Algorithms/is-anagram/submission-8.py
@@ -0,0 +1,19 @@
+
+        
+            
+class Solution:
+    def isAnagram(self, s: str, t: str) -> bool:
+        if len(s)!=len(t):
+            return False
+        count={}
+        for c in s:
+            count[c]=count.get(c,0)+1
+        for c in t:
+            if c not in count:
+                return False
+            count[c]-=1
+        for value in count.values():
+            if value != 0:
+                return False
+        
+        return True
